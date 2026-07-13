18cf734eeafc1d23ac2443940920b8927c221afb
Christos Daglaroglou <christos.daglaroglou@gmail.com>
2026-07-13 19:53:40 +0300

    refactor: migrate project from next-on-pages to open-next for Cloudflare deployment and remove edge runtime constraints

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextRequest, NextFetchEvent } from "next/server";
+
+const isProtectedRoute = createRouteMatcher(["/editor(.*)", "/dashboard(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth.protect({
+      unauthenticatedUrl: new URL("/sign-in", req.url).toString(),
+    });
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+    "/__clerk/:path*",
+  ],
+};
